b457f9f56c0bce5802acd9a1e1f4205e2b496492
Fahadzz4 <fahad.k.alfaleh@gmail.com>
2018-05-18 15:28:31 -0300

    added a button, text field and a function

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void addWordOnClick(View v){
+        EditText edit = (EditText)findViewById(R.id.editText); // edit text field
+
+        TextView textv = (TextView)findViewById(R.id.textView); // the view text field
+
+        String result = edit.getText().toString(); // getting the text in edit text field
+
+        textv.setText(result); // setting the word to the view text
     }
 }
